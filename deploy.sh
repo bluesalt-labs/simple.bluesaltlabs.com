15536cdccb809bc455fbcc569aced3ab1f26a00e
Luke Sontrop <luke@bluesaltlabs.com>
2024-10-17 00:19:35 -0700

    Adds initial files for github pages deployment

---
+++ deploy.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# build
+npm run build
+
+# navigate into the build output directory
+cd dist
+
+# if you are deploying to a custom domain
+echo 'simple.bluesaltlabs.com' > CNAME
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
+git push -f git@github.com:bluesalt-labs/simple.bluesaltlabs.com.git master:gh-pages
+
+cd -
